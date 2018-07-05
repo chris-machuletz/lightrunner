fb93206826739752c7749e46b520f6304945b99a
chris-machuletz <machuletz.christoph@gmail.com>
2018-07-05 20:28:23 +0200

    Einfache Streckengenerierung und Spielersteuerung
    Die Strecke wird nun vor dem Spieler generiert und hinter ihm zerstört. Die Streckenabschnitte sind vorerst leer und es wird auch immer der selbe Abschnitt generiert. Der Spieler kann nach rechts und links gesteuert werden.

---
+++ Neon Runner/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour {
+
+    private CharacterController controller;
+    private Vector3 moveVector;
+
+    private float velocity = 10.0f;
+    private float gravity = 10.0f;
+    private float verticalVelocity = 0.0f;
+
+    private float animationDuration = 2.0f; // Verhindern, dass das Schiff bewegt wird, wenn die Kamera-Animation läuft
+
+	// Use this for initialization
+	void Start () {
+        this.transform.Translate(0, 0.5f, 0);
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.time < animationDuration) // Verhindern, dass das Schiff bewegt wird, wenn die Kamera-Animation läuft
+        {
+            controller.Move(Vector3.forward * Time.deltaTime * velocity);
+            return;
+        }
+
+        moveVector = Vector3.zero;
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -0.2f;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        // X-Movement
+        moveVector.x = Input.GetAxisRaw("Horizontal") * velocity * 2;
+
+        // Y-Movement
+        moveVector.y = verticalVelocity;
+
+
+        // Z-Movement
+        moveVector.z = velocity;
+
+        controller.Move(moveVector * Time.deltaTime);
+	}
+}
---
+++ Neon Runner/Assets/Scripts/TrackSpawnManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackSpawnManager : MonoBehaviour {
+
+    public GameObject[] trackSections;
+    private Transform playerTransform;
+    private float spawnZ = -15.0f; // -15, damit beim Start keine Lücke hinterm Spieler zu sehen ist
+    private float trackSectionLength = 60.0f;
+    private int amountofRenderedTracks = 4;
+    private float safeZone = 90.0f; // Löscht Streckensegmente erst, nachdem Spieler sie passiert hat
+
+    private List<GameObject> activeTracks;
+
+    // Use this for initialization
+    void Start()
+    {
+        activeTracks = new List<GameObject>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        
+        for(int i = 0; i < amountofRenderedTracks; i++)
+        {
+            SpawnTrack();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerTransform.position.z > (spawnZ + safeZone - amountofRenderedTracks * trackSectionLength))
+        {
+            SpawnTrack();
+            DeleteTrack();
+        }
+    }
+
+    void SpawnTrack(int trackIndex = -1)
+    {
+        GameObject gameobj;
+        gameobj = Instantiate(trackSections[0]) as GameObject;
+        gameobj.transform.SetParent(transform);
+        gameobj.transform.position = Vector3.forward * spawnZ;
+        spawnZ += trackSectionLength;
+        activeTracks.Add(gameobj);
+    }
+
+    void DeleteTrack()
+    {
+        Destroy(activeTracks[0]);
+        activeTracks.RemoveAt(0);
+    }
+}
