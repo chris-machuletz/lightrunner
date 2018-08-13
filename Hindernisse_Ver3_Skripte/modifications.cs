5fed20aee0934fb90f3e69d20c1078d0579076b1
Fresh-Phil <40402721+Fresh-Phil@users.noreply.github.com>
2018-08-13 19:14:25 +0200

    Update für die zufälligen Hindernisse (Ver.3) (Skripte)
    neu: die Hindernisse können sich nun bewegen, allerdings noch etwas verbuggt; das Selbstkollisions-Skript wurde deaktiviert

---
+++ Hindernisse_Ver3_Skripte/modifications.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class modifications : MonoBehaviour {
+
+    public float movSpeed = 5f; //Geschwindigkeit mit der sich das Objekt bewegt
+    public delegate void mov(); // erschafft delegate, der es erlaubt funktionen wie variablen zu nutzen
+    public List<mov> moves = new List<mov>(); // liste von funktionen
+
+    public int tposX = 5; // Grenze für Verschiebung nach rechts
+    public int tposXneg = -5; // Grenze für Verschiebung nach links
+    public int tposY = 5; // Grenze für Verschiebung nach oben
+    public int tposYneg = -5; // Grenze für Verschiebung nach unten
+
+    Vector3 tarVecXpos; //Vektor für Zielposition bei X (positiv)
+    Vector3 tarVecXneg; //Vektor für Zielposition bei X (negativ)
+    Vector3 tarVecYpos; //Vektor für Zielposition bei Y (positiv)
+    Vector3 tarVecYneg; //Vektor für Zielposition bei X (negativ)
+
+    bool xrb = false; //Boolean für die Auslösung von XmoveRight
+    bool xlb = false; //Boolean für die Auslösung von XmoveLeft
+    bool xbb = false;
+    bool yub = false; //Boolean für die Auslösung von YmoveUp
+    bool ydb = false; //Boolean für die Auslösung von YmoveDown
+    bool ybb = false;
+
+    public GameObject obj; //Platzhalterobjekt
+
+
+
+    // Use this for initialization
+    public void Start () {
+        
+        //Hinzufügen der jeweiligen Funktionen in die Liste
+        moves.Add(XmoveRight);
+        moves.Add(XmoveLeft);
+        moves.Add(XmoveBoth);
+        moves.Add(YmoveUp);
+        moves.Add(YmoveDown);
+        moves.Add(YmoveBoth);
+        moves.Add(Nothing1);
+        moves.Add(Nothing2);
+        moves.Add(Nothing3);
+        moves.Add(Nothing4);
+
+        Initialize(); //Aufrufen der Initialisierungsfunktion
+
+    }
+
+    public void Initialize()
+    {
+        obj = gameObject; //setzt das Objekt hier mit dem eigentlichen Obstacle gleich
+
+        tarVecXpos = new Vector3(obj.transform.position.x + tposX, 0, 0); //Ort der Verschiebung (aktuelle Position + tposX)
+        tarVecXneg = new Vector3(obj.transform.position.x + tposXneg, 0, 0); //Ort der Verschiebung (aktuelle Position + tposXneg)
+        tarVecYpos = new Vector3(0, obj.transform.position.y + tposY, 0); //Ort der Verschiebung (aktuelle Position + tposY)
+        tarVecYneg = new Vector3(0, obj.transform.position.x + tposYneg, 0); //Ort der Verschiebung (aktuelle Position + tposYneg)
+
+        //PROBLEM BEI DEN ZIELPOSITIONEN: SIE SCHEINEN MIT GAMEOBJECT DAS EMPTY ZU MEINEN, NICHT DIE ERZEUGTEN HINDERNISSE AUS DER LISTE...
+        //DADURCH VERSCHIEBEN SIE SICH OFT AUF DIE GLEICHE POSITION...
+        //ANSATZ: DIE OBJEKTE SO ANSPRECHEN WIE BEI DER SELBSTKOLLISION
+
+        int randi = Random.Range(0, moves.Count - 1); //erstellt eine Zufallszahl welche sich im Bereich der Größe der Funktionsliste befindet
+
+        //Debug.Log("randi: " + randi);
+
+        moves[randi](); //führt die Funktion an der Stelle "randi" aus EIGENTLICH UNNÖTIG?!
+
+        if (randi == 0)
+        {
+            xrb = true; //sorgt dafür dass die Funktion die in der Update wartet, ausgeführt wird
+            //Debug.Log("XmoveRight");
+        }
+
+        if (randi == 1)
+        {
+            xlb = true; //sorgt dafür dass die Funktion die in der Update wartet, ausgeführt wird
+            //Debug.Log("XmoveLeft");
+        }
+
+        if (randi == 2)
+        {
+            xbb = true; //sorgt dafür dass die Funktion die in der Update wartet, ausgeführt wird
+            //Debug.Log("XmoveBoth");
+        }
+
+        if (randi == 3)
+        {
+            yub = true; //sorgt dafür dass die Funktion die in der Update wartet, ausgeführt wird
+            //Debug.Log("YmoveUp");
+        }
+
+        if (randi == 4)
+        {
+            ydb = true; //sorgt dafür dass die Funktion die in der Update wartet, ausgeführt wird
+            //Debug.Log("YmoveDown");
+        }
+
+        if (randi == 5)
+        {
+            ybb = true; //sorgt dafür dass die Funktion die in der Update wartet, ausgeführt wird
+            //Debug.Log("YmoveBoth");
+        }
+
+
+    }
+
+
+    public void Nothing1()
+    {
+        Debug.Log("You shall not move!"); //ist ein Puffer sodass nicht jedes Hindernis sich bewegt
+    }
+
+    public void Nothing2()
+    {
+        Debug.Log("You shall not move!"); //ist ein Puffer sodass nicht jedes Hindernis sich bewegt
+    }
+
+    public void Nothing3()
+    {
+        Debug.Log("You shall not move!"); //ist ein Puffer sodass nicht jedes Hindernis sich bewegt
+    }
+
+    public void Nothing4()
+    {
+        Debug.Log("You shall not move!"); //ist ein Puffer sodass nicht jedes Hindernis sich bewegt
+    }
+
+    //NOTIZ: ES GIBT SICHERLICH BESSERE LÖSUNG FÜR DIE NOTHING MEHTODEN, ZUM BEISPIEL DASS DIE MODIFIKATIONEN ZUFÄLLIG AUF DIE HINDERNISSE ANGEWANDT WERDEN
+
+
+
+    public void XmoveRight()
+    {
+        obj.transform.position = Vector3.MoveTowards(obj.transform.position, tarVecXpos, movSpeed * Time.deltaTime); //sorgt für Bewegung des Objekts in Richtung der Zielposition
+    }
+
+    public void XmoveLeft()
+    {
+        obj.transform.position = Vector3.MoveTowards(obj.transform.position, tarVecXneg, movSpeed * Time.deltaTime); //sorgt für Bewegung des Objekts in Richtung der Zielposition
+    }
+
+    public void XmoveBoth()
+    {
+        XmoveRight();
+        XmoveLeft();
+        //ruft beide Richtungen auf, bewegt sich hin und her (x mal)
+    }
+
+    public void YmoveUp()
+    {
+        obj.transform.position = Vector3.MoveTowards(obj.transform.position, tarVecYpos, movSpeed * Time.deltaTime); //sorgt für Bewegung des Objekts in Richtung der Zielposition
+    }
+
+    public void YmoveDown()
+    {
+        obj.transform.position = Vector3.MoveTowards(obj.transform.position, tarVecYneg, movSpeed * Time.deltaTime); //sorgt für Bewegung des Objekts in Richtung der Zielposition
+    }
+
+        public void YmoveBoth()
+    {
+        YmoveUp();
+        YmoveDown();
+        //ruft beide Richtungen auf, bewegt sich von Oben nach Unten (x mal)
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+        if (xrb == true) //löst die entsprechende Funktion nur aus, wenn sie auch ausgewählt wurde
+        {
+            if (obj.transform.position != tarVecXpos) //Funktion wird solange ausgeführt, bis es die Zielposition erreicht hat
+            {
+                XmoveRight();
+            }
+        }
+
+
+        if (xlb == true) //löst die entsprechende Funktion nur aus, wenn sie auch ausgewählt wurde
+        {
+            if (obj.transform.position != tarVecXneg) //Funktion wird solange ausgeführt, bis es die Zielposition erreicht hat
+            {
+                XmoveLeft();
+            }
+        }
+
+
+        if (xbb == true) //löst die entsprechende Funktion nur aus, wenn sie auch ausgewählt wurde
+        {
+            if (obj.transform.position != tarVecXpos) //Funktion wird solange ausgeführt, bis es die Zielposition erreicht hat
+            {
+                XmoveRight();
+            }
+
+            if (obj.transform.position != tarVecXneg) //Funktion wird solange ausgeführt, bis es die Zielposition erreicht hat
+            {
+                XmoveLeft();
+            }
+        }
+
+
+        if (yub == true) //löst die entsprechende Funktion nur aus, wenn sie auch ausgewählt wurde
+        {
+            if (obj.transform.position != tarVecYpos) //Funktion wird solange ausgeführt, bis es die Zielposition erreicht hat
+            {
+                YmoveUp();
+            }
+        }
+
+
+        if (ydb == true) //löst die entsprechende Funktion nur aus, wenn sie auch ausgewählt wurde
+        {
+            if (obj.transform.position != tarVecYneg) //Funktion wird solange ausgeführt, bis es die Zielposition erreicht hat
+            {
+                YmoveDown();
+            }
+        }
+
+
+        if (ybb == true) //löst die entsprechende Funktion nur aus, wenn sie auch ausgewählt wurde
+        {
+            if (obj.transform.position != tarVecYpos) //Funktion wird solange ausgeführt, bis es die Zielposition erreicht hat
+            {
+                YmoveUp();
+            }
+
+            if (obj.transform.position != tarVecYneg) //Funktion wird solange ausgeführt, bis es die Zielposition erreicht hat
+            {
+                YmoveDown();
+            }
+        }
+
+
+
+    }
+}
---
+++ Hindernisse_Ver3_Skripte/randomSpawn.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomSpawn : MonoBehaviour {
+
+    public float min = 0.1f; //mindestzeit die vergeht bis zum nächsten Spawn
+    public float max = 0.5f; //höchstzeit die zum nächsten Spawn vergeht
+    public GameObject obstacl; //ein nutzbares Gameobject
+    public float ranx, rany, ranz; //achsen-werte die zufällig sein sollen
+    public float borderx = 3f; //obere Grenze für zufälligen x-wert
+    public float bordery = 0f; //obere Grenze für y-wert
+    public float borderz = 5f; //obere Grenze für zufälligen z-wert
+    public Vector3 posVec; //Vector für neue Position
+    public List<GameObject> obstacls = new List<GameObject>(); //macht die erzeugten gameObjects zugreifbar/ speichert sie
+    public int obstcount; //Listenzähler --> nicht benutzt
+
+
+    public float RandomizeX() //setzt den zu verwendenden x-wert zufällig
+    {
+        ranx = Random.Range(-borderx, borderx);
+        return ranx;
+    }
+
+    public float RandomizeY() //setzt den zu verwendenden x-wert zufällig
+    {
+        rany = Random.Range(0, bordery);
+        return rany;
+    }
+
+    public float RandomizeZ() //setzt den zu verwendenden z-wert zufällig
+    {
+        ranz = Random.Range(-borderz, borderz);
+        return ranz;
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    { 
+        //Invoke("Instanciate", (Random.Range(min, max))); //ruft die fkt zu zufälligen Zeiten auf, was dazu führt, 
+                                                           //dass zufällig viele Instanzen der Hindernisse zu zufälligen Zeiten gespawnt werden
+
+        //ruft nur einmal zu Beginn die Hindernissfkt. auf und erzeugt i+1 Instanzen von ihnen
+        for(int i = 0; i<=5; i++)
+        {
+            Instanciate();
+        }
+
+    }
+
+    public void Instanciate()
+    {
+        float rant = Random.Range(min, max); //random time im bereich von min - max
+
+        // hier dazwischen den code für das spawnobject einfügen
+
+
+        obstacl = GetComponent<cub>().Create(); //ruft create funktion von cub auf und damit dessen gameObject
+
+        obstacl.AddComponent<modifications>(); //fügt Skript mit Modifikationen hinzu
+        obstacl.GetComponent<modifications>().Start();
+
+        //obstacl.AddComponent<col_self>(); // fügt selbstkollisions-script hinzu EDIT: RAUSGENOMMEN WEIL HINDERLICH/ VERWIRREND BEI BEWEGL. HINDERNISSEN
+        obstacls.Add(obstacl);  //fügt grade erschaffenes obstacle der Liste hinzu
+
+        //obstcount = obstacls.Count-1; //funktioniert nur für das zum Zeitpunkt des Löschens aktuellste erschaffene Obstacle... nicht nacheinander in der Reihenfolge
+        //obstcount++;
+
+        //Code für zufällige Position aufrufen
+        RandomizeX();
+        RandomizeY();
+        RandomizeZ();
+       
+        posVec = new Vector3(ranx, rany, ranz);//speichern von zufälligen Werten in Vector
+        obstacl.transform.position += posVec;//zufällige Position zuweisen
+        obstacl.transform.localScale += new Vector3(0, Random.Range(0, 4), 0); //skaliert die obstacles zufällig
+
+
+        //Invoke("Instanciate", rant); //ruft sich selber nochmal auf --> nicht benötigt, wenn die Hindernisse alle auf einmal spawnen sollen
+    }
+
+}
+
+
+
