83fb517d7133f6c2125f122f9bbe6bf30d0c98b9
Abraxius <40402725+Abraxius@users.noreply.github.com>
2018-08-17 16:03:36 +0200

    Hauptmenü
    Hinzufügen eines simplen Hauptmenüs

---
+++ Neon Runner/Assets/Scripts/Hauptmenü.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hauptmenü : MonoBehaviour {
+
+    //Script für die Funktionen des Hauptmenüs
+
+    //Start Button
+    public void Spielstarten()
+    {
+        Application.LoadLevel(0);
+    }
+
+    //Highscore Button
+    public void BesterPunktestand()
+    {
+        Application.LoadLevel(2);
+    }
+
+    //Exit Button
+    //Dieser Button funktioniert nur wenn das Spiel fertig compiliert ist
+    public void Spielbeenden()
+    {
+        Application.Quit();
+    }
+}
