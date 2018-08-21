7e483dfbdd66431fd63520ff594e72c77d1ad652
Abraxius <40402725+Abraxius@users.noreply.github.com>
2018-08-21 13:17:40 +0200

    Hauptmenü
    Hauptmenü wurde optimiert, alle Funktionen gehen nun.
    Gibt vor zurück, Highscore Reset und Anzeige und beenden.
    Spieler läuft rukkel frei und aus irgendeinem Grund hat die Map nun Material.

--- Neon Runner/Assets/Scripts/Hauptmenü.cs
+++ Neon Runner/Assets/Scripts/Hauptmenü.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hauptmenü : MonoBehaviour {
 
     //Script für die Funktionen des Hauptmenüs
+    public Highscore otherscript;   //ermöglicht den Zugriff auf die Bool im anderen C#
+    public Text highScore;
 
     //Start Button
     public void Spielstarten()
     {
-        Application.LoadLevel(0);
+        Application.LoadLevel(1);
     }
 
     //Highscore Button
@@ -18,11 +21,29 @@ public class Hauptmenü : MonoBehaviour {
         Application.LoadLevel(2);
     }
 
+    //Bringt zum Hauptmenü zurück
+    public void Zurück()
+    {
+
+        Application.LoadLevel(0);
+    }
     //Exit Button
     //Dieser Button funktioniert nur wenn das Spiel fertig compiliert ist
     public void Spielbeenden()
     {
         Application.Quit();
+    }
+
+    //Highscore Anzeige
+    private void Start()
+    {
+        highScore.text = PlayerPrefs.GetInt("HighScore").ToString();
+    }
+
+    public void Reset()     //reset funktion per button des highscores
+    {
+        PlayerPrefs.DeleteKey("HighScore");
+        highScore.text = "0";
     }
 }
 
