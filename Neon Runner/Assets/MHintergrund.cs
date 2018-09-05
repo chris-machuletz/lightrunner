3fcbb38bf502d446f7a1f7e7d036d6ed0d89380f
Abraxius <40402725+Abraxius@users.noreply.github.com>
2018-09-05 21:56:33 +0200

    Menü Hintergrund
    Bei jedem Menü wurde ein laufender Hintergrund hinzugefügt.
    Er zeigt die Map wie Sie ohne Raumschiff durchflogen wird.
    Problem:
    Jedoch wird die Hintergrund Map immer nur einmal angezeigt, danach wird
    Sie nicht mehr gespawnt.. WARUM???

---
+++ Neon Runner/Assets/MHintergrund.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MHintergrund : MonoBehaviour {
+
+    public float vorwärtsspeed = 50.0f; //z
+    public float speed = 100;    //x
+    public float gravity = 10;
+    public float jumppower = 10; //sprunghöhe
+    public float test = 0;
+
+
+    bool hover = true;
+    public bool inputHover = false;
+    bool inputJump = false;
+    float velocity = 0;
+    Vector3 moveDirection = Vector3.zero;
+    CharacterController characterController;
+
+    // Use this for initialization
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Move()
+    {
+        moveDirection.z = vorwärtsspeed;
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        moveDirection.x = velocity;
+        moveDirection.y -= gravity * Time.deltaTime; //erzeugt eine Gravitation für den Charakter
+        characterController.Move(moveDirection * Time.deltaTime);   //deltaTime damit die bewegungen nicht von PC unterschiedlich sind
+    }
+
+    void FixedUpdate()
+    {
+        vorwärtsspeed = vorwärtsspeed + 0.01f;    //beschleunigung mit der zeit
+        Move();
+    }
+}
---
+++ Neon Runner/Assets/Scripts/MenüBewegung.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenüBewegung : MonoBehaviour {
+
+    public float n = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.Rotate(20, 0, 0);
+    }
+
+    void FixedUpdate()
+    {
+        n = n + 1;
+        transform.position = new Vector3(0, 10, n);
+
+    }
+}
