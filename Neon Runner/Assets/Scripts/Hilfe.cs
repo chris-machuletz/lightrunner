e3cf18183b0dfeae6de0fc2f26b939bebf9fe457
Abraxius <40402725+Abraxius@users.noreply.github.com>
2018-09-14 00:14:13 +0200

    Minimale Optische Änderungen

--- Neon Runner/Assets/Scripts/Hilfe.cs
+++ Neon Runner/Assets/Scripts/Hilfe.cs
@@ -21,6 +21,11 @@ public class Hilfe : MonoBehaviour {
     private GameObject LumenCube;
     private GameObject lightGameObject3;
 
+    //Unverwundbar
+    public Material SafeMats;
+    private GameObject SafeCube;
+    private GameObject lightGameObject4;
+
     // Use this for initialization
     void Start () {
 
@@ -28,11 +33,11 @@ public class Hilfe : MonoBehaviour {
         HoverCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         HoverCube.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         HoverCube.GetComponent<Renderer>().material = HoverMats;
-        HoverCube.transform.position = new Vector3(-8, 18, 0);
+        HoverCube.transform.position = new Vector3(-5, 18, 8);
         HoverCube.transform.Rotate(25, 0, 0);
         HoverCube.name = "HoverUp";
 
-        lightGameObject1 = new GameObject("The Light");
+        lightGameObject1 = new GameObject("Green Light");
         Light lightComp1 = lightGameObject1.AddComponent<Light>();
         lightComp1.color = Color.green;
         lightComp1.intensity = 8.5f;
@@ -43,35 +48,55 @@ public class Hilfe : MonoBehaviour {
         LebenCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         LebenCube.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         LebenCube.GetComponent<Renderer>().material = LebenMats;
-        LebenCube.transform.position = new Vector3(-8, 18, 0);
+        LebenCube.transform.position = new Vector3(-5, 16, 8);
         LebenCube.transform.Rotate(25, 0, 0);
         LebenCube.name = "LebenUp";
 
-        lightGameObject2 = new GameObject("The Ligh2t");
+        lightGameObject2 = new GameObject("Red Light");
         Light lightComp2 = lightGameObject2.AddComponent<Light>();
         lightComp2.color = Color.red;
         lightComp2.intensity = 8.5f;
         lightComp2.transform.localScale = new Vector3(2.5f, 2.5f, 2.5f);
         lightGameObject2.transform.position = LebenCube.transform.position;
 
+        //Unverwundbar cube
+        SafeCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        SafeCube.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        SafeCube.GetComponent<Renderer>().material = SafeMats;
+        SafeCube.transform.position = new Vector3(-5, 14, 8);
+        SafeCube.transform.Rotate(25, 0, 0);
+        SafeCube.name = "SafeUp";
+
+        lightGameObject4 = new GameObject("Blue Light");
+        Light lightComp4 = lightGameObject4.AddComponent<Light>();
+        lightComp4.color = Color.blue;
+        lightComp4.intensity = 8.5f;
+        lightComp4.transform.localScale = new Vector3(2.5f, 2.5f, 2.5f);
+        lightGameObject4.transform.position = SafeCube.transform.position;
+
         //lumen cube
         LumenCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         LumenCube.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         LumenCube.GetComponent<Renderer>().material = LumenMats;
-        LumenCube.transform.position = new Vector3(-8, 18, 0);
+        LumenCube.transform.position = new Vector3(-5, 12, 8);
         LumenCube.transform.Rotate(25, 0, 0);
-        LumenCube.name = "HoverUp";
+        LumenCube.name = "LumenUp";
 
-        lightGameObject3 = new GameObject("The Light3");
+        lightGameObject3 = new GameObject("White Light");
         Light lightComp3 = lightGameObject3.AddComponent<Light>();
         lightComp3.color = Color.white;
         lightComp3.intensity = 8.5f;
         lightComp3.transform.localScale = new Vector3(2.5f, 2.5f, 2.5f);
         lightGameObject3.transform.position = LumenCube.transform.position;
+
+
     }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        HoverCube.transform.Rotate(0, 1, 0);
+        LebenCube.transform.Rotate(0, 1, 0);
+        LumenCube.transform.Rotate(0, 1, 0);
+        SafeCube.transform.Rotate(0, 1, 0);
+    }
 }
