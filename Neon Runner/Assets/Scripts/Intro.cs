a87b6d0bbfff7e994b5650009bbade4b8acc9ab5
Abraxius <40402725+Abraxius@users.noreply.github.com>
2018-09-23 21:55:42 +0200

    Intro
    -kleine Cutfehler beim Introladen behoben

--- Neon Runner/Assets/Scripts/Intro.cs
+++ Neon Runner/Assets/Scripts/Intro.cs
@@ -5,26 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class Intro : MonoBehaviour {
 
-    public Animator transitionAnim;
-    //public string sceneName;
+    public GameObject video;
+    public GameObject fakebutton;
+    public GameObject richtigebutton;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        //StartCoroutine(LoadScene());
+        StartCoroutine(Einlauf());
+        StartCoroutine(ButtonEinlauf());
+    }
 	
 	// Update is called once per frame
 	void Update () {
-       // if (Input.GetKeyDown(KeyCode.Space))
-       // {
-            StartCoroutine(LoadScene());
-            //SceneManager.LoadScene(sceneName);
-       // }
-	}
+
+    }
 
     IEnumerator LoadScene()
     {
-        transitionAnim.SetTrigger("end");
         yield return new WaitForSeconds(2.8f);
         SceneManager.LoadScene(1);
+    }
+    IEnumerator Einlauf()
+    {
+        yield return new WaitForSeconds(2.8f);
+        video.SetActive(false);
+    }
+    IEnumerator ButtonEinlauf()
+    {
+        yield return new WaitForSeconds(3.1f);
+        richtigebutton.SetActive(true);
+        fakebutton.SetActive(false);
+
     }
 }
